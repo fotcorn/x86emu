30df4c81a7b6d4637e8ad4386c5f9c29ca910093
fotcorn <fotcorn@gmail.com>
2017-02-10 00:16:20 +0100

    test: ignore size suffixes

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,12 @@ cat $1 > temp/out.asm
 echo "nop" >> temp/out.asm
 as temp/out.asm -o temp/out.o
 ld temp/out.o -o temp/out
-objdump -d temp/out | tail -n +8 | cut -d$'\t' -f3 | head -n -1 > temp/dis_objdump.asm
+objdump -d temp/out | tail -n +8 | cut -d$'\t' -f3 | head -n -1 | \
+sed -e 's/movl/mov /g' | \
+sed -e 's/callq/call /' | \
+sed -e 's/addl/add /g' | \
+sed -e 's/leaveq/leave/g' | \
+sed -e 's/retq/ret/g' \
+> temp/dis_objdump.asm
 cargo run temp/out _start > temp/dis_emu.asm
 diff -u temp/dis_objdump.asm temp/dis_emu.asm
