005694461b665de219346fd9d225dd3a12bd5051
fotcorn <fotcorn@gmail.com>
2017-02-13 01:46:42 +0100

    tests: added direction test file

---
+++ test/direction.asm
@@ -0,0 +1,13 @@
+.text
+.global  _start
+_start:
+
+mov    -0x4(%ebp),%eax
+mov    -0x4(%ebp),%rax
+mov    -0x4(%rbp),%eax
+mov    -0x4(%rbp),%rax
+
+mov    %eax,-0x4(%ebp)
+mov    %rax,-0x4(%ebp)
+mov    %eax,-0x4(%rbp)
+mov    %rax,-0x4(%rbp)
