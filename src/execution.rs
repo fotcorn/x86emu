cfb1125ca85a8c4762d57de97b377d51ead2c3e9
fotcorn <fotcorn@gmail.com>
2017-03-01 03:36:25 +0100

    execution: implemented argument size logic

--- src/execution.rs
+++ src/execution.rs
@@ -1,4 +1,4 @@
-use instruction_set::InstructionArgument;
+use instruction_set::{InstructionArgument, Register};
 
 use cpu::CPU;
 
@@ -6,16 +6,53 @@ pub enum ArgumentSize {
     Bit64,
     Bit32,
     Bit16,
+    Bit8,
 }
 
 
 impl CPU {
     pub fn first_argument_size(&self, arg: &InstructionArgument) -> ArgumentSize {
-        ArgumentSize::Bit64
+        match *arg {
+            InstructionArgument::OneRegister { ref register, .. } => get_register_size(register),
+            InstructionArgument::TwoRegister { ref register1,
+                                               ref register2,
+                                               reverse_direction,
+                                               .. } => {
+                if reverse_direction {
+                    get_register_size(register2)
+                } else {
+                    get_register_size(register1)
+                }
+            }
+            InstructionArgument::Immediate8 { .. } => ArgumentSize::Bit8,
+            InstructionArgument::Immediate32 { .. } => ArgumentSize::Bit32,
+            InstructionArgument::Immediate8BitRegister { .. } => ArgumentSize::Bit8,
+            InstructionArgument::Immediate32BitRegister { .. } => ArgumentSize::Bit32,
+        }
     }
 
-    pub fn second_argument_size(&self, arg: InstructionArgument) -> ArgumentSize {
-        ArgumentSize::Bit64
+    pub fn second_argument_size(&self, arg: &InstructionArgument) -> ArgumentSize {
+        match *arg {
+            InstructionArgument::OneRegister { .. } => panic!("Only one argument available"),
+            InstructionArgument::TwoRegister { ref register1,
+                                               ref register2,
+                                               reverse_direction,
+                                               .. } => {
+                if reverse_direction {
+                    get_register_size(register1)
+                } else {
+                    get_register_size(register2)
+                }
+            }
+            InstructionArgument::Immediate8 { .. } => panic!("Only one argument available"),
+            InstructionArgument::Immediate32 { .. } => panic!("Only one argument available"),
+            InstructionArgument::Immediate8BitRegister { ref register, .. } => {
+                get_register_size(register)
+            }
+            InstructionArgument::Immediate32BitRegister { ref register, .. } => {
+                get_register_size(register)
+            }
+        }
     }
 
     pub fn first_argument_i8(&self, arg: InstructionArgument) -> i8 {
@@ -66,4 +103,17 @@ pub fn convert_i32_to_u8vec(value: i32) -> Vec<u8> {
 
 pub fn convert_i64_to_u8vec(value: i64) -> Vec<u8> {
     vec![]
+}
+
+
+fn get_register_size(reg: &Register) -> ArgumentSize {
+    match *reg {
+        Register::RAX | Register::RBX | Register::RCX | Register::RDX | Register::RSP |
+        Register::RBP | Register::RSI | Register::RDI | Register::RIP => ArgumentSize::Bit64,
+        Register::EAX | Register::EBX | Register::ECX | Register::EDX | Register::ESP |
+        Register::EBP | Register::ESI | Register::EDI => ArgumentSize::Bit32,
+        Register::ES | Register::CS | Register::SS | Register::DS | Register::FS | Register::GS => {
+            ArgumentSize::Bit16
+        }
+    }
 }
