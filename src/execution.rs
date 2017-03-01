95b15f20b38e85fca2bbebef8cad9727aabb9803
fotcorn <fotcorn@gmail.com>
2017-03-01 15:27:52 +0100

    execution: added initial stack operations

--- src/execution.rs
+++ src/execution.rs
@@ -184,26 +184,46 @@ impl CPU {
 
 
     // stack operations
-    pub fn stack_push(&self, data: Vec<u8>) {
-        panic!("Not implemented");
+    pub fn stack_push(&mut self, data: Vec<u8>) {
+        for v in data {
+            self.rsp -= 1;
+            self.stack[self.rsp as usize] = v;
+        }
     }
-
 }
 
-pub fn convert_i8_to_u8vec(value: i32) -> Vec<u8> {
-    vec![]
+pub fn convert_i8_to_u8vec(value: i8) -> Vec<u8> {
+    vec![value as u8]
 }
 
-pub fn convert_i16_to_u8vec(value: i32) -> Vec<u8> {
-    vec![]
+pub fn convert_i16_to_u8vec(value: i16) -> Vec<u8> {
+    vec![
+        (value & 0x00FF) as u8,
+        (value & 0xFF00 >> 8) as u8,
+    ]
 }
 
 pub fn convert_i32_to_u8vec(value: i32) -> Vec<u8> {
-    vec![]
+    vec![
+        (value & 0x000000FF) as u8,
+        (value & 0x0000FF00 >> 8) as u8,
+        (value & 0x00FF0000 >> 16) as u8,
+        (value & 0xFF000000 >> 24) as u8,
+    ]
 }
 
 pub fn convert_i64_to_u8vec(value: i64) -> Vec<u8> {
-    vec![]
+    vec![
+        (value & 0x00000000000000FF) as u8,
+        (value & 0x000000000000FF00 >> 8) as u8,
+        (value & 0x0000000000FF0000 >> 16) as u8,
+        (value & 0x00000000FF000000 >> 24) as u8,
+
+        (value & 0x000000FF00000000 >> 32) as u8,
+        (value & 0x0000FF0000000000 >> 40) as u8,
+        (value & 0x00FF000000000000 >> 48) as u8,
+        (value & 0xFF00000000000000 >> 56) as u8,
+    ]
 }
 
 
