30bf67ee546b260ced5881f593325ee0e85b494c
fotcorn <fotcorn@gmail.com>
2017-03-02 02:37:53 +0100

    execution: fixed warnings

--- src/execution.rs
+++ src/execution.rs
@@ -55,13 +55,13 @@ impl CPU {
         }
     }
 
-    pub fn first_argument_i8(&self, arg: &InstructionArgument) -> i8 {
+    /*pub fn first_argument_i8(&self, arg: &InstructionArgument) -> i8 {
         panic!("Not implemented");
     }
 
     pub fn first_argument_i16(&self, arg: &InstructionArgument) -> i16 {
         panic!("Not implemented");
-    }
+    }*/
 
     pub fn first_argument_i32(&self, arg: &InstructionArgument) -> i32 {
         match *arg {
@@ -94,7 +94,7 @@ impl CPU {
     }
 
 
-    pub fn second_argument_i8(&self, arg: &InstructionArgument) -> i8 {
+    /*pub fn second_argument_i8(&self, arg: &InstructionArgument) -> i8 {
         panic!("Not implemented");
     }
 
@@ -108,7 +108,7 @@ impl CPU {
 
     pub fn second_argument_i64(&self, arg: &InstructionArgument) -> i64 {
         panic!("Not implemented");
-    }
+    }*/
 
 
     // register operations
@@ -234,37 +234,37 @@ impl CPU {
 
 }
 
-pub fn convert_i8_to_u8vec(value: i8) -> Vec<u8> {
+/*pub fn convert_i8_to_u8vec(value: i8) -> Vec<u8> {
     vec![value as u8]
 }
 
 pub fn convert_i16_to_u8vec(value: i16) -> Vec<u8> {
     vec![
-        (value & 0x00FF) as u8,
-        (value & 0xFF00 >> 8) as u8,
+        (value as u16 & 0x00FF) as u8,
+        (value as u16 & 0xFF00 >> 8) as u8,
     ]
-}
+}*/
 
 pub fn convert_i32_to_u8vec(value: i32) -> Vec<u8> {
     vec![
-        (value & 0x000000FF) as u8,
-        (value & 0x0000FF00 >> 8) as u8,
-        (value & 0x00FF0000 >> 16) as u8,
-        (value & 0xFF000000 >> 24) as u8,
+        (value as u32 & 0x000000FF) as u8,
+        (value as u32 & 0x0000FF00 >> 8) as u8,
+        (value as u32 & 0x00FF0000 >> 16) as u8,
+        (value as u32 & 0xFF000000 >> 24) as u8,
     ]
 }
 
 pub fn convert_i64_to_u8vec(value: i64) -> Vec<u8> {
     vec![
-        (value & 0x00000000000000FF) as u8,
-        (value & 0x000000000000FF00 >> 8) as u8,
-        (value & 0x0000000000FF0000 >> 16) as u8,
-        (value & 0x00000000FF000000 >> 24) as u8,
+        (value as u64 & 0x00000000000000FF) as u8,
+        (value as u64 & 0x000000000000FF00 >> 8) as u8,
+        (value as u64 & 0x0000000000FF0000 >> 16) as u8,
+        (value as u64 & 0x00000000FF000000 >> 24) as u8,
 
-        (value & 0x000000FF00000000 >> 32) as u8,
-        (value & 0x0000FF0000000000 >> 40) as u8,
-        (value & 0x00FF000000000000 >> 48) as u8,
-        (value & 0xFF00000000000000 >> 56) as u8,
+        (value as u64 & 0x000000FF00000000 >> 32) as u8,
+        (value as u64 & 0x0000FF0000000000 >> 40) as u8,
+        (value as u64 & 0x00FF000000000000 >> 48) as u8,
+        (value as u64 & 0xFF00000000000000 >> 56) as u8,
     ]
 }
 
