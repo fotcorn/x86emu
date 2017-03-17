e71cb109b493f08cff9b0c6b052f63c5525372ab
fotcorn <fotcorn@gmail.com>
2017-03-17 10:41:32 +0100

    implemented Display trait for InstructionArgument(s)

--- src/instruction_set.rs
+++ src/instruction_set.rs
@@ -131,85 +131,27 @@ impl InstructionArguments {
 
 impl fmt::Display for InstructionArguments {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-        /*
+        match self.second_argument {
+            Some(ref second_argument) => write!(f, "{} {}", self.first_argument, second_argument),
+            None => write!(f, "{}", self.first_argument),
+        }
+    }
+}
+
+impl fmt::Display for InstructionArgument {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            InstructionArgument::OneRegister { ref register, .. } => write!(f, "{}", register),
-            InstructionArgument::TwoRegister { ref register1,
-                                               ref register2,
-                                               effective_address_displacement,
-                                               reverse_direction } => {
-                match effective_address_displacement {
-                    Some(displacement) if displacement > 0 => {
-                        if reverse_direction {
-                            write!(f, "{:#x}({}),{}", displacement, register1, register2)
-                        } else {
-                            write!(f, "{},{:#x}({})", register2, displacement, register1)
-                        }
-                    }
-                    Some(displacement) if displacement < 0 => {
-                        if reverse_direction {
-                            write!(f, "-{:#x}({}),{}", displacement * -1, register1, register2)
-                        } else {
-                            write!(f, "{},-{:#x}({})", register2, displacement * -1, register1)
-                        }
-                    }
-                    Some(_) => {
-                        if reverse_direction {
-                            write!(f, "({}),{}", register1, register2)
-                        } else {
-                            write!(f, "{},({})", register2, register1)
-                        }
-                    }
-                    None => {
-                        if reverse_direction {
-                            write!(f, "{},{}", register1, register2)
-                        } else {
-                            write!(f, "{},{}", register2, register1)
-                        }
-                    }
+            InstructionArgument::Register { ref register } => write!(f, "{}", register),
+            InstructionArgument::Immediate { immediate } => write!(f, "{:x}", immediate),
+            InstructionArgument::EffectiveAddress { ref register, displacement } => {
+                if displacement < 0  {
+                    write!(f, "-{:#x}({})", displacement, register)
+                } else if displacement > 0 {
+                    write!(f, "{:#x}({})", displacement, register)
+                } else {
+                    write!(f, "({})", register)
                 }
-            }
-            InstructionArgument::Immediate8 { immediate } => write!(f, "{:x}", immediate),
-            InstructionArgument::Immediate32 { immediate } => write!(f, "{:x}", immediate),
-            InstructionArgument::Immediate32BitRegister { ref register,
-                                                          immediate,
-                                                          effective_address_displacement,
-                                                          .. } => {
-                match effective_address_displacement {
-                    Some(displacement) if displacement > 0 => {
-                        write!(f, "${:#x},{:#x}({})", immediate, displacement, register)
-                    }
-                    Some(displacement) if displacement < 0 => {
-                        write!(f,
-                               "${:#x},-{:#x}({})",
-                               immediate,
-                               displacement * -1,
-                               register)
-                    }
-                    Some(_) => write!(f, "${:#x},({})", immediate, register),
-                    None => write!(f, "${:#x},{}", immediate, register),
-                }
-            }
-            InstructionArgument::Immediate8BitRegister { ref register,
-                                                         immediate,
-                                                         effective_address_displacement,
-                                                         .. } => {
-                match effective_address_displacement {
-                    Some(displacement) if displacement > 0 => {
-                        write!(f, "${:#x},{:#x}({})", immediate, displacement, register)
-                    }
-                    Some(displacement) if displacement < 0 => {
-                        write!(f,
-                               "${:#x},-{:#x}({})",
-                               immediate,
-                               displacement * -1,
-                               register)
-                    }
-                    Some(_) => write!(f, "${:#x},({})", immediate, register),
-                    None => write!(f, "${:#x},{}", immediate, register),
-                }
-            }
-        }*/
+            },
+        }
     }
 }
