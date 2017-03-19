d4d94edfa04baf8bc7ef773ae15889ab934b8d0b
fotcorn <fotcorn@gmail.com>
2017-03-19 23:25:31 +0100

    utils: fixed i32/i64 conversion to u8 vector

--- src/utils.rs
+++ src/utils.rs
@@ -11,19 +11,19 @@ pub fn convert_i16_to_u8vec(value: i16) -> Vec<u8> {
 
 pub fn convert_i32_to_u8vec(value: i32) -> Vec<u8> {
     vec![(value as u32 & 0x000000FF) as u8,
-         (value as u32 & 0x0000FF00 >> 8) as u8,
-         (value as u32 & 0x00FF0000 >> 16) as u8,
-         (value as u32 & 0xFF000000 >> 24) as u8]
+         ((value as u32 & 0x0000FF00) >> 8) as u8,
+         ((value as u32 & 0x00FF0000) >> 16) as u8,
+         ((value as u32 & 0xFF000000) >> 24) as u8]
 }
 
 pub fn convert_i64_to_u8vec(value: i64) -> Vec<u8> {
     vec![(value as u64 & 0x00000000000000FF) as u8,
-         (value as u64 & 0x000000000000FF00 >> 8) as u8,
-         (value as u64 & 0x0000000000FF0000 >> 16) as u8,
-         (value as u64 & 0x00000000FF000000 >> 24) as u8,
+         ((value as u64 & 0x000000000000FF00) >> 8) as u8,
+         ((value as u64 & 0x0000000000FF0000) >> 16) as u8,
+         ((value as u64 & 0x00000000FF000000) >> 24) as u8,
 
-         (value as u64 & 0x000000FF00000000 >> 32) as u8,
-         (value as u64 & 0x0000FF0000000000 >> 40) as u8,
-         (value as u64 & 0x00FF000000000000 >> 48) as u8,
-         (value as u64 & 0xFF00000000000000 >> 56) as u8]
+         ((value as u64 & 0x000000FF00000000) >> 32) as u8,
+         ((value as u64 & 0x0000FF0000000000) >> 40) as u8,
+         ((value as u64 & 0x00FF000000000000) >> 48) as u8,
+         ((value as u64 & 0xFF00000000000000) >> 56) as u8]
 }
