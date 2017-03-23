fbaedc8b29d6be899287dbcf72b233177567e821
fotcorn <fotcorn@gmail.com>
2017-03-23 20:25:12 +0100

    linux: execute code at 0x100000

--- src/loader/linux.rs
+++ src/loader/linux.rs
@@ -8,7 +8,7 @@ use cpu::cpu_trait::CPU;
 const SETUP_HEADER_OFFSET: usize = 0x1F1;
 const BIT64_OFFSET: i64 = 0x200;
 const ZERO_PAGE_ADDRESS: u64 = 0x10000;
-const LOAD_ADDRESS: u64 = 0x100000;
+const LOAD_ADDRESS: i64 = 0x100000;
 
 /* see <linux kernel source>/Documentation/x86/boot.txt and zero-page.txt
  * for documentation of the 64 bit boot protocol
@@ -27,17 +27,13 @@ pub fn linux(filename: &str, cpu: &CPU) {
     machine_state.mem_write(ZERO_PAGE_ADDRESS + 0x1F1, setup_header);
     machine_state.rsi = ZERO_PAGE_ADDRESS as i64;
 
+    // load code at 0x100.000
     // count of setup sects is the first value in the setup header struct
     let setup_sect = buffer[SETUP_HEADER_OFFSET];
     let offset = (setup_sect + 1) as usize * 512;
 
-    // load code at address 0
-    // TODO: how much code do we need to load?
-    machine_state.mem_write(0, &buffer[offset..offset+0x1000]);
-    machine_state.rip = BIT64_OFFSET;
-
-    // load code at 0x100.000
-    machine_state.mem_write(LOAD_ADDRESS, &buffer[offset..]);
+    machine_state.mem_write(LOAD_ADDRESS as u64, &buffer[offset..]);
+    machine_state.rip = LOAD_ADDRESS + BIT64_OFFSET;
 
     // start execution
     let mut decoder = Decoder::new(cpu, &mut machine_state);
