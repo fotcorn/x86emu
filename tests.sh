c7ec8cd073c6a3cd6bdeabfc38ae31c687fed8d2
fotcorn <fotcorn@gmail.com>
2017-04-14 02:02:01 +0200

    tests: moved to central script for easy local execution

---
+++ tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+test/decoder/test.sh test/decoder/direction.asm
+test/decoder/test.sh test/decoder/effective_address.asm
+test/decoder/test.sh test/decoder/generated.asm
+test/decoder/test.sh test/decoder/hello.asm
+test/decoder/test.sh test/decoder/register_address.asm
+test/decoder/test.sh test/decoder/rex_b.asm
+test/decoder/test.sh test/decoder/rex_modrm.asm
+test/decoder/test.sh test/decoder/rex.asm
+test/decoder/test.sh test/decoder/rip_relative.asm
+test/execution/test.sh test/execution/jc_jnc.S
+test/execution/test.sh test/execution/call_ret.S
