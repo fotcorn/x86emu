244c308dbb01371e2d51ea307ef4a5833da9d0ea
fotcorn <fotcorn@gmail.com>
2017-04-15 18:28:08 +0200

    test: make jump test script executable from root

--- test/jumps/jumps.sh
+++ test/jumps/jumps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p temp
-python jumps.py > jumps.S
-as jumps.S -o temp/jumps.o
+python test/jumps/jumps.py > temp/jumps.S
+as temp/jumps.S -o temp/jumps.o
 ld -o temp/jumps temp/jumps.o
 ./temp/jumps
 cargo run -- --loader elf --cpu emu temp/jumps  --symbol _start
