b924505f3178ebefdfd3dc9c1104c699ed1d3ae6
fotcorn <fotcorn@gmail.com>
2017-04-18 01:57:07 +0200

    qemu: added breakpoint offsets, linux boot command line

--- linux/gdb.sh
+++ linux/gdb.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 gdb-multiarch \
 	-ex "target remote localhost:1234" \
-	-ex "break *0x100200" \
+	-ex "break *0x2476d20" \
 	-ex "set step-mode on" \
 	-ex "c" \
 	-ex "disconnect" \
 	-ex "set architecture i386:x86-64:intel" \
-	-ex "target remote localhost:1234"
+	-ex "target remote localhost:1234" \
+	-ex "layout regs"
+
+# 0x10026b
+# 0x24735d0
+# 0x24735ea
+# 0x2476450
+# 0x24764fa
+# 0x24770ed
+# 0x2476fe4
+# 0x2476d20
+# 0x2476d48
+# 0x2476db6
+# 0x2476dc4
--- linux/qemu.sh
+++ linux/qemu.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-qemu-system-x86_64 -kernel linux/arch/x86/boot/bzImage  -S -s -no-kvm
+qemu-system-x86_64 -kernel linux/arch/x86/boot/bzImage -append "debug earlyprintk=vga" -S -s -no-kvm
