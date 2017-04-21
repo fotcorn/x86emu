d181c58017d238d0db17542a757211ae85c2e5c1
fotcorn <fotcorn@gmail.com>
2017-04-21 14:07:58 +0200

    tests: use tmp directory for easier shell autocompletion

--- test/c_execution/test.sh
+++ test/c_execution/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-mkdir -p temp/
+mkdir -p tmp/
 
-gcc $1 -o temp/out
-gcc -c $1 -o temp/out.o
-./temp/out
-cargo run -- --loader elf --cpu emu temp/out --symbol main
+gcc $1 -o tmp/out
+gcc -c $1 -o tmp/out.o
+./tmp/out
+cargo run -- --loader elf --cpu emu tmp/out --symbol main
