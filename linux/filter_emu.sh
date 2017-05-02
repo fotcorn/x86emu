f486539b29801838f46a6e683b261bf14a4a909f
fotcorn <fotcorn@gmail.com>
2017-05-02 23:54:18 +0200

    qemu: updated filter scripts

--- linux/filter_emu.sh
+++ linux/filter_emu.sh
@@ -5,12 +5,15 @@ sed -e 's/ja.*/ja/g' | \
 sed -e 's/jae.*/jae/g' | \
 sed -e 's/jnc.*/jnc/g' | \
 sed -e 's/jz.*/jz/g' | \
+sed -e 's/jg.*/jg/g' | \
 sed -e 's/jbe.*/jbe/g' | \
 sed -e 's/jnz.*/jnz/g' | \
 sed -e 's/jle.*/jle/g' | \
 sed -e 's/call.*/call/g' | \
+sed -e 's/VIDEO.*//g' | \
 sed -e 's/0x0(/(/g' | \
 sed -e '/^$/d' | \
 sed -e 's/[ \t]*$//' | \
+sed -e 's/mov    %eax,%rax/cltq/g' | \
 sed -e 's/jmp.*/jmp/g' \
 > emu.txt
--- linux/filter_gdb.sh
+++ linux/filter_gdb.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 python filter_gdb.py | \
-sed -e 's/movl/mov /g' | \
-sed -e 's/movs[bw][wlq]/movsx /g' | \
-sed -e 's/movz[bw][wlq]/movzx /g' | \
-sed -e 's/movq/mov /g' | \
+sed -e 's/mov[lqbw]/mov /g' | \
+sed -e 's/movs[bwl][wlq]/movsx /g' | \
+sed -e 's/movz[bwl][wlq]/movzx /g' | \
 sed -e 's/movabs/mov   /g' | \
 sed -e 's/andb/and /g' | \
 sed -e 's/cmpb/cmp /g' | \
@@ -16,6 +15,7 @@ sed -e 's/ja.*/ja/g' | \
 sed -e 's/jne.*/jnz/g' | \
 sed -e 's/jbe.*/jbe/g' | \
 sed -e 's/jle.*/jle/g' | \
+sed -e 's/jg.*/jg/g' | \
 sed -e 's/call.*/call/g' | \
 sed -e 's/jmp.*/jmp/g' | \
 sed -e 's/0x0(/(/g' | \
