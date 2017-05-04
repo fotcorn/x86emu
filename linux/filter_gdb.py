7cf10b07904d9ae28b585aa40c595a48b1ad9291
fotcorn <fotcorn@gmail.com>
2017-05-05 00:27:19 +0200

    qemu: cleaned up filter_gdb.py script

--- linux/filter_gdb.py
+++ linux/filter_gdb.py
@@ -9,8 +9,16 @@ for line in lines:
         continue
 
     match = re.match('=> 0x[a-z0-9]+:\t(.*)\n', line)
-    if not match:
+    if match:  # instruction
+        line = match.group(1)
+        if line == 'rep stos %rax,%es:(%rdi)':
+            if stos:
+                continue
+            else:
+                stos = True
+    else:  # register value
         b = False
+        # skip some uninteresting registers
         for x in ['cs', 'ss', 'ds', 'es', 'fs', 'gs', 'eflags']:
             if line.startswith(x):
                 b = True
@@ -18,18 +26,9 @@ for line in lines:
         if b:
             continue
 
+        # only print hex value of register
         match = re.match('([a-z0-9]+ +0x[a-f0-9]+).*', line)
         if match:
             line = match.group(1)
-
-        print(line.strip())
-        continue
-    
-    line = match.group(1)
-    if line == 'rep stos %rax,%es:(%rdi)':
-        if stos:
-            continue
-        else:
-            stos = True
 
     print(line.strip())
