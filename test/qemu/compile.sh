15579dbc14af79e18cb35f54969b28f0f901cede
fotcorn <fotcorn@gmail.com>
2017-07-14 21:21:29 +0200

    tests: qemu test file diff output from native with emulator

--- test/qemu/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-diet gcc test-i386.c -o test
-cargo run -- --loader elf test  --symbol _start
---
+++ test/qemu/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir -p tmp
+diet gcc test-i386.c -o tmp/out
+./tmp/out | cut -c 12- | tr "[:upper:]" "[:lower:]" > tmp/native.txt
+cargo run -- --loader elf tmp/out  --symbol _start | cut -c 12- | tr "[:upper:]" "[:lower:]" > tmp/emulator.txt
+diff -u tmp/emulator.txt tmp/native.txt | diff-so-fancy --color
