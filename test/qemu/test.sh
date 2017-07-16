8908988f72fd0d363d6cd2486fdc2b2170ca0365
fotcorn <fotcorn@gmail.com>
2017-07-16 14:57:54 +0200

    tests: hack for qemu test file no longer needed

--- test/qemu/test.sh
+++ test/qemu/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p tmp
 diet gcc test-i386.c -o tmp/out
-./tmp/out | cut -c 12- | tr "[:upper:]" "[:lower:]" > tmp/native.txt
-cargo run --release -- --loader elf tmp/out  --symbol _start | cut -c 12- | tr "[:upper:]" "[:lower:]" > tmp/emulator.txt
+./tmp/out | tr "[:upper:]" "[:lower:]" > tmp/native.txt
+cargo run --release -- --loader elf tmp/out  --symbol _start | tr "[:upper:]" "[:lower:]" > tmp/emulator.txt
 diff -u tmp/emulator.txt tmp/native.txt | diff-so-fancy --color
